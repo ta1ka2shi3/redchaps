8bb12062f7fcc0b675d8150e90b7aef42d1810ae
ta1ka2shi3 <kosugetakashi@icloud.com>
2020-08-04 22:22:53 +0900

    show メソッドの導入

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "games#index"
-  resources :games, only:[:new, :create, :index, :show]
+  resources :games do
+    get "/games/:id", to: "games#show"
+  end
   #resources :users,  only: [:index, :new, :create, :destroy]
 end
